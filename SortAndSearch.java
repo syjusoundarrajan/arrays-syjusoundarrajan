e8ee5028ed049025e0092b2547bb9a2fdc05daf9
Nana <nana.d@hotmail.de>
2019-02-20 13:06:22 +0100

    added exercises

---
+++ SortAndSearch.java
@@ -0,0 +1,10 @@
+
+public class BubbleSort {
+	
+	//todo: Write a method bubbleSrt which gets an integer array as input and which sorts this array according to the bubblesort algorithm
+	//If you don't know how bubblesort works you can read it here: http://www.pkirs.utep.edu/CIS3355/Tutorials/chapter9/tutorial9A/bubblesort.htm
+	//Now write another method binSrch. This method again receives an integer array as input together with another integer n.
+	//First, it sorts this array by using the bubblSrt method. Then, it searches for n.
+	//You can look up the binary search algorithm here: https://www.khanacademy.org/computing/computer-science/algorithms/binary-search/a/binary-search
+
+}
